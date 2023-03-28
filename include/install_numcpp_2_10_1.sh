88e26d51e5d073da20a48c804604b092db2162b2
vietanh112 <imnova1212@gmail.com>
2023-03-28 09:38:39 +0700

    update numcpp v2.10.1

--- include/install_numcpp_2_10_1.sh
+++ include/install_numcpp_2_10_1.sh
@@ -6,8 +6,8 @@ cd ~/Documents/Cpp/SuperCpp
 # chuyển đến folder chứa thư viện
 mkdir -p include && cd include
 
-URL="https://github.com/dpilger26/NumCpp/archive/refs/tags/Version_2.10.0.zip"
-FILENAME="Version_2.10.0.zip"
+URL="https://github.com/dpilger26/NumCpp/archive/refs/tags/Version_2.10.1.zip"
+FILENAME="Version_2.10.1.zip"
 wget -N $URL
 
 unzip -q $FILENAME
