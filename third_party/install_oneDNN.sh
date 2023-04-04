4e08a65c9283b92b20a0318b911c53818c830b6d
vietanh112 <imnova1212@gmail.com>
2023-04-04 17:34:52 +0700

    CONDAENV=cxx

--- third_party/install_oneDNN.sh
+++ third_party/install_oneDNN.sh
@@ -5,5 +5,5 @@ CONDAENV="cxx"    # edit conda env name
 conda install -c conda-forge onednn --force-reinstall --name "$CONDAENV" -y
 
 # https://github.com/oneapi-src/oneDNN#installation
-# header file tại `${CONDA_PREFIX}/include` #! ~/anaconda3/envs/cpp/include
-# lib tại         `${CONDA_PREFIX}/lib`     #! ~/anaconda3/envs/cpp/lib
+# header file tại `${CONDA_PREFIX}/include` #! ~/anaconda3/envs/$CONDAENV/include
+# lib tại         `${CONDA_PREFIX}/lib`     #! ~/anaconda3/envs/$CONDAENV/lib
