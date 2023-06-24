67b43657a741d0ca817f2c5a7c71c2f1d292ac59
agent12 <imnova1212@gmail.com>
2023-06-24 18:32:50 +0700

    numcpp 2.11.0 & boost 1.82.0

--- third_party/install_boost_1_82_0.sh
+++ third_party/install_boost_1_82_0.sh
@@ -3,7 +3,7 @@ workspaceFolder=~/Documents/Cxx/SuperCpp
 cd "$workspaceFolder"
 
 #1. Update Ubuntu Linux
-sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove
+sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
 
 #2. Install the Developer’s tool
 sudo apt-get install build-essential g++ python3-dev autotools-dev libicu-dev libbz2-dev libboost-all-dev -y
@@ -11,13 +11,13 @@ sudo apt-get install build-essential g++ python3-dev autotools-dev libicu-dev li
 #3. Download the Boost C++ library
 # Visit the Website `https://www.boost.org/`` and click on the Download button.
 mkdir -p third_party && cd third_party
-wget https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.gz
+wget https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.tar.gz
 
 #4. Extract the Tar file
-tar xvf boost_1_81_0.tar.gz
+tar xvf boost_1_82_0.tar.gz
 
 #5. Switch to the extracted directory
-cd boost_1_81_0
+cd boost_1_82_0
 
 #6. Setup Boost’s bootstrap
 sudo ./bootstrap.sh --prefix=/usr/
@@ -25,7 +25,7 @@ sudo ./bootstrap.sh --prefix=/usr/
 #7. Install Boost on Ubuntu 20.04  or 22.04
 #Building the Boost C++ Libraries
 sudo ./b2 install
-cd ../.. && sudo rm -rf third_party/boost_1_81_0 
+cd ../.. && sudo rm -rf third_party/boost_1_82_0
 #8. Use Boost library in C++ programming
 
 # #include <iostream>
--- third_party/install_numcpp_2_11_0.sh
+++ third_party/install_numcpp_2_11_0.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-# edit lại working dir 
+# edit lại working dir
 workspaceFolder=~/Documents/Cxx/SuperCpp
 
 cd "$workspaceFolder"
 # chuyển đến folder chứa thư viện
 mkdir -p third_party && cd third_party
 
-URL="https://github.com/dpilger26/NumCpp/archive/refs/tags/Version_2.10.1.zip"
-FILENAME="Version_2.10.1.zip"
+URL="https://github.com/dpilger26/NumCpp/archive/refs/tags/Version_2.11.0.zip"
+FILENAME="Version_2.11.0.zip"
 wget -N $URL
 
 unzip -q $FILENAME
