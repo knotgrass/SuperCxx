4550bf13b18c566f726f09487e91fc005d31ce22
agent12 <imnova1212@gmail.com>
2023-07-04 22:10:10 +0700

    install gtest

--- third_party/install_gtest_1_13_0.sh
+++ third_party/install_gtest_1_13_0.sh
@@ -11,4 +11,19 @@ wget -O googletest-1.13.0.zip https://github.com/google/googletest/archive/refs/
 unzip -q googletest-1.13.0.zip
 rm googletest-1.13.0.zip
 
+cd googletest-1.13.0
+mkdir install
+mkdir build
+cd build
+
+# Install in /usr/local/ by default
+# https://github.com/google/googletest/blob/main/googletest/README.md
+# cmake ../
+
+# https://github.com/google/googletest/issues/868#issuecomment-245593106
+cmake ../ -DCMAKE_INSTALL_PREFIX=$workspaceFolder/third_party/googletest-1.13.0/install
+
+
+make -j$(nproc)
+sudo make install
 cd "$workspaceFolder"
