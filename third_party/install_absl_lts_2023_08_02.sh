a78faee7afac58b6652c701061ba575dceeb0808
agent12 <imnova1212@gmail.com>
2023-08-14 23:10:48 +0700

    absl 2023_08_02

--- third_party/install_absl_lts_2023_08_02.sh
+++ third_party/install_absl_lts_2023_08_02.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # edit lại working dir
-workspaceFolder=/home/tz/Documents/programming/SuperCxx
+workspaceFolder="$(dirname "$(dirname "$(realpath "$0")")")"
 
 cd "$workspaceFolder"
 
@@ -9,7 +9,7 @@ cd "$workspaceFolder"
 mkdir -p third_party && cd third_party
 
 # https://abseil.io/docs/cpp/tools/cmake-installs
-git clone https://github.com/abseil/abseil-cpp.git -b lts_2023_01_25
+git clone https://github.com/abseil/abseil-cpp.git -b lts_2023_08_02
 cd abseil-cpp
 
 echo "\n"
@@ -17,9 +17,10 @@ echo "Start build Absl"
 
 mkdir -p build && cd build
 cmake .. \
-    -DABSL_BUILD_TESTING=ON \
+    -DABSL_BUILD_TESTING=OFF \
     -DABSL_USE_GOOGLETEST_HEAD=ON \
     -DCMAKE_CXX_STANDARD=17 \
+    -DABSL_PROPAGATE_CXX_STD=ON \
     -DCMAKE_INSTALL_PREFIX=$workspaceFolder/third_party/abseil-cpp/install
 
 cmake --build . --target all -j$(nproc)
