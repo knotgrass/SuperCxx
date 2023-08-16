f664d1dd4d4e4f926680502d9423c570e880509d
agent12 <imnova1212@gmail.com>
2023-08-16 13:48:40 +0700

    fix DABSL_BUILD_TESTING=ON

--- third_party/install_absl_lts_2023_08_02.sh
+++ third_party/install_absl_lts_2023_08_02.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
-# edit lại working dir
 workspaceFolder="$(dirname "$(dirname "$(realpath "$0")")")"
 
 cd "$workspaceFolder"
 
-# chuyển đến folder chứa thư viện
 mkdir -p third_party && cd third_party
 
 # https://abseil.io/docs/cpp/tools/cmake-installs
@@ -17,11 +15,13 @@ echo "Start build Absl"
 
 mkdir -p build && cd build
 cmake .. \
-    -DABSL_BUILD_TESTING=OFF \
-    -DABSL_USE_GOOGLETEST_HEAD=ON \
+    -DABSL_BUILD_TESTING=ON \
+    -DABSL_LOCAL_GOOGLETEST_DIR=$workspaceFolder/third_party/googletest-1.14.0 \
     -DCMAKE_CXX_STANDARD=17 \
     -DABSL_PROPAGATE_CXX_STD=ON \
     -DCMAKE_INSTALL_PREFIX=$workspaceFolder/third_party/abseil-cpp/install
+# -DABSL_GOOGLETEST_DOWNLOAD_URL=https://github.com/google/googletest/archive/refs/tags/v1.14.0.zip \
+# https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/HEAD/CMake/README.md
 
 cmake --build . --target all -j$(nproc)
 ctest
